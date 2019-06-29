400cd4ca63860532faf56b344c8578d4a8ffa434
熊乾坤 <2372389705@qq.com>
2019-06-29 11:01:58 +0800

    2019.2.24

--- test/Test.java
+++ test/Test.java
@@ -3,7 +3,7 @@ import java.sql.Date;
 /**
  * FunctionalTest
  */
-public class FunctionalTest {
+public class Test {
 
     public static void main(String[] args) {
         System.out.println(new Date(System.currentTimeMillis()));
