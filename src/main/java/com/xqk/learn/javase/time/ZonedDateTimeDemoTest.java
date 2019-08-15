35e3bf8a701babc290209e3b15ff284760deac52
xiongqiankun@kedacom.com <xiongqiankun@kedacom.com>
2019-08-15 17:35:25 +0800

    2019-8-14 下午-新增java 8的Period类

--- src/main/java/com/xqk/learn/javase/time/ZonedDateTimeDemoTest.java
+++ src/main/java/com/xqk/learn/javase/time/ZonedDateTimeDemoTest.java
@@ -10,6 +10,8 @@ import java.time.*;
  *
  * ZonedDateTime表示带时区的LocalDateTime,其中包含了时区的信息
  *
+ * ZoneId.of可以使用两种方式：GMT+08:00或者Asia/Shanghai
+ *
  * @author 熊乾坤
  */
 @Slf4j
@@ -19,7 +21,12 @@ class ZonedDateTimeDemoTest {
         //LocalDateTime->ZonedDateTime
         LocalDateTime ldt = LocalDateTime.now();
         ZonedDateTime zdt = ldt.atZone(ZoneId.of("GMT+8"));
+        ZonedDateTime zdt1 = ldt.atZone(ZoneId.of("GMT+09:00"));
+        ZonedDateTime zdt2 = ldt.atZone(ZoneId.of("Europe/Paris"));
+        log.info(ldt.toString());
         log.info(zdt.toString());
+        log.info(zdt1.toString());
+        log.info(zdt2.toString());
 
         //LocalDate->ZonedDateTime
         LocalDate ld = LocalDate.now();
