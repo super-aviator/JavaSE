eefce52aa9717876aef693afca2802ac58d3e660
熊乾坤 <2372389705@qq.com>
2019-08-15 13:21:21 +0800

    2019-8-15 中午-新增部分java 8的时间API

---
+++ src/main/java/com/xqk/learn/javase/time/DurationTest.java
@@ -0,0 +1,27 @@
+package com.xqk.learn.javase.time;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+/**
+ * @author 熊乾坤
+ */
+@Slf4j
+class DurationTest {
+
+    /**
+     * Duration表示一段时间间隔，可以使用静态方法去创建两个LocalDateTime、LocalTime、Instant之间的间隔
+     * 值得注意的是Duration不能表示两个LocalDate之间的间隔,否则会抛UnsupportedTemporalTypeException异常
+     */
+    @Test
+    void testCreateDuration() {
+        LocalDateTime ld1 = LocalDateTime.of(2019, 8, 1, 10, 10, 10);
+        LocalDateTime ld2 = LocalDateTime.of(2019, 8, 15, 9, 10, 10);
+        Duration duration = Duration.between(ld1, ld2);
+        log.info(String.valueOf(duration.toDays()));
+        log.info(String.valueOf(duration.toHours()));
+    }
+}
--- src/main/java/com/xqk/learn/javase/time/LocalDateTimeTest.java
+++ src/main/java/com/xqk/learn/javase/time/LocalDateTimeTest.java
@@ -1,22 +1,95 @@
 package com.xqk.learn.javase.time;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
-import java.util.logging.Logger;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * 包括LocalDateTime,LocalDate,LocalTime三个不可变类
- * <p>
  * LocalDateTimeTest
+ * <p>
+ * LocalDate不可变，仅仅表示日期，并且不带时区，
  *
  * @author 熊乾坤
  */
-public class LocalDateTimeTest {
-    private static Logger log = Logger.getLogger("LocalDateTimeTest");
+@Slf4j
+class LocalDateTimeTest {
 
-    public static void main(String[] args) {
 
+    @Test
+    void testCreateLocalDate() {
+        //创建当前时间
+        LocalDate.now();
+
+        //使用of创建LocalDate对象
+        LocalDate localDate1 = LocalDate.of(2019, 8, 14);
+        LocalDate localDate2 = LocalDate.of(2019, Month.AUGUST, 14);
+        assertEquals(localDate1, localDate2);
+
+        //使用parse方法构建LocalDate,注意，MM需要在月份前面加0,特别注意
+        LocalDate localDate3 = LocalDate.parse("2019-08-14", new DateTimeFormatterBuilder().appendPattern("yyyy-MM-dd").toFormatter());
+        LocalDate localDate4 = LocalDate.parse("2019-08-14", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate localDate5 = LocalDate.parse("2019-08-14", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        assertEquals(localDate3, localDate4);
+        assertEquals(localDate3, localDate5);
+
+        //LocalDate默认支持的格式为yyyy-MM-dd,其他格式会抛出DateTimeParseException
+        LocalDate localDate7 = LocalDate.parse("2019-08-15");
+        log.info(localDate7.toString());
+
+
+        //使用DayOfYear创建LocalDate
+        LocalDate localDate6 = LocalDate.ofYearDay(2019, LocalDate.now().getDayOfYear());
+        assertEquals(LocalDate.now(), localDate6);
+    }
+
+    @Test
+    void testCreateLocalDateTime() {
+        LocalDateTime ldt1 = LocalDateTime.of(2019, 8, 15, 10, 3, 20);
+        LocalDate ld = LocalDate.of(2019, 8, 15);
+        LocalTime lt = LocalTime.of(10, 3, 20);
+        LocalDateTime ldt2 = LocalDateTime.of(ld, lt);
+        assertEquals(ldt1, ldt2);
+    }
+
+    @Test
+    void testGetField() {
+        //通过get方法和枚举类ChronoField获取字段
+        LocalDate localDate = LocalDate.now();
+        int year = localDate.get(ChronoField.YEAR);
+        int month = localDate.get(ChronoField.MONTH_OF_YEAR);
+        int day = localDate.get(ChronoField.DAY_OF_MONTH);
+        log.info("{}-{}-{}", year, month, day);
+    }
+
+    /**
+     * 因为时间类是不可变的，可以通过with方法获取某个域修改之后的对象。
+     */
+    @Test
+    void testModifyField() {
+        LocalDate localDate = LocalDate.now();
+        LocalDate localDate1 = localDate.withYear(2020);
+        log.info(localDate.toString());
+        log.info(localDate1.toString());
+        log.info(localDate.with(ChronoField.DAY_OF_MONTH, 1).toString());
+
+        LocalTime localTime = LocalTime.now();
+        LocalTime localTime1 = localTime.withHour(11);
+        log.info(localTime.toString());
+        log.info(localTime1.toString());
+        log.info(localTime.with(ChronoField.HOUR_OF_DAY, 1).toString());
+    }
+
+    @Test
+    void testFormat() {
         // 格式化和自定义格式化,星期为E
         LocalDateTime date = LocalDateTime.now();
         log.info(date.format(DateTimeFormatter.ISO_DATE));
