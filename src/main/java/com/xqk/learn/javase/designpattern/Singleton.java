eb3880c3e451bfd4a3c2e1cbda9d975fda8398ae
熊乾坤 <2372389705@qq.com>
2019-08-16 17:31:58 +0800

    2019-8-14 下午-新增设计模式中的单利模式类

---
+++ src/main/java/com/xqk/learn/javase/designpattern/Singleton.java
@@ -0,0 +1,11 @@
+package com.xqk.learn.javase.designpattern;
+
+/**
+ * The type Singleton. 单例设计模式
+ * <p>
+ * 单利设计模式的主要目的是确保类的实例在内存中是唯一的，一个类有且仅有一个对象
+ *
+ * @author 熊乾坤
+ */
+public class Singleton {
+}
