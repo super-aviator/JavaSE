45f6d87bb496c85b433d74d1e6cd2ff37eabb5c2
熊乾坤 <2372389705@qq.com>
2019-08-21 18:43:50 +0800

    2019-8-21 晚上-完成服用章节的学习，感觉自己又离菜逼更近了一部步

--- src/main/java/com/xqk/learn/javase/reuse/FinalClass.java
+++ src/main/java/com/xqk/learn/javase/reuse/FinalClass.java
@@ -1,7 +1,7 @@
 package com.xqk.learn.javase.reuse;
 
 /**
- * final 类禁止继承，类中所有的方法都被隐式地指定为 final，所以没有办法覆写它们。
+ * final 类禁止继承，类中所有的方法都被隐式地指定为 final，所以没有办法重写它们。
  * 你可以在 final 类中的方法加上 final 修饰符，但不会增加任何意义。
  *
  * @author 熊乾坤
